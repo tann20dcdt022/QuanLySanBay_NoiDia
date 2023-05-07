d99421ed58a6663ee74cec72c5e1761fbbd3ffa4
tanvt <tannguyenvt231101@gmail.com>
2023-05-07 09:54:08 +0700

    07/05/2023 Đã hoàn thành thêm, sửa, xóa danh sách Máy bay (Câu a)

--- quanlicacchuyenbaynoidia/Data/Planes.cs
+++ quanlicacchuyenbaynoidia/Data/Planes.cs
@@ -16,5 +16,43 @@ namespace quanlicacchuyenbaynoidia.Data
             this.planes = new Plane[300];
             this.size = 0;
         }
+
+        public void addPlane(Plane plane)
+        {
+            this.planes[this.size] = plane;
+            this.size++;
+        }
+
+        public void removePlane(string soHieuMayBay) 
+        {
+            int index = 0;
+            for(int i = 0; i < this.size; i++)
+            {
+                if (this.planes[i].soHieuMayBay == soHieuMayBay)
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            for(int i = index; i < this.size; i++)
+            {
+                this.planes[i] = this.planes[i + 1];
+            }
+
+            this.size--;
+        }
+
+        public void editPlane(Plane plane)
+        {
+            for (int i = 0; i < this.size; i++)
+            {
+                if (this.planes[i].soHieuMayBay == plane.soHieuMayBay)
+                {
+                    this.planes[i] = plane;
+                    break;
+                }
+            }
+        }
     }
 }
